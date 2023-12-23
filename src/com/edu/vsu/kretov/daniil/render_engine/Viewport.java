d940ac3a177a6fcd2605166eef02ceee79fef84a
Khanin Dmitriy <dmitrii.khanin@gmail.com>
2023-12-23 19:59:38 +0300

    Added viewport

---
+++ src/com/edu/vsu/kretov/daniil/render_engine/Viewport.java
@@ -0,0 +1,13 @@
+package com.edu.vsu.kretov.daniil.render_engine;
+
+import javax.swing.*;
+
+public class Viewport extends JPanel {
+    public Viewport() {
+
+    }
+
+    public void drawPixel(int x, int y) {
+
+    }
+}
